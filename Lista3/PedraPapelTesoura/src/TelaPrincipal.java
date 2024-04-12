8388b0b7e932a192d39240c417ae088580e17127
Felype <felypeperesecher@gmail.com>
2024-04-12 10:54:47 -0300

    adicionando trabalho pedra, papel, tesoura.

--- Lista3/PedraPapelTesoura/src/TelaPrincipal.java
+++ Lista3/PedraPapelTesoura/src/TelaPrincipal.java
@@ -1,8 +1,10 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-
 /**
  *
  * @author 05136425016
@@ -14,7 +16,6 @@ public class TelaPrincipal extends javax.swing.JFrame {
      */
     public TelaPrincipal() {
         initComponents();
-        
 
     }
 
@@ -23,38 +24,47 @@ public class TelaPrincipal extends javax.swing.JFrame {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
-        JbPedra = new javax.swing.JRadioButton();
-        JbPapel = new javax.swing.JRadioButton();
-        JbTesoura = new javax.swing.JRadioButton();
+        JrPedra = new javax.swing.JRadioButton();
+        JrPapel = new javax.swing.JRadioButton();
+        JrTesoura = new javax.swing.JRadioButton();
+        JbJogar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setPreferredSize(new java.awt.Dimension(800, 600));
 
         jLabel1.setText("Pedra, Papel ou Tesoura?");
 
-        JbPedra.setText("Pedra");
-        JbPedra.addActionListener(new java.awt.event.ActionListener() {
+        JrPedra.setText("Pedra");
+        JrPedra.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                JbPedraActionPerformed(evt);
+                JrPedraActionPerformed(evt);
             }
         });
 
-        JbPapel.setText("Papel");
+        JrPapel.setText("Papel");
 
-        JbTesoura.setText("Tesoura");
+        JrTesoura.setText("Tesoura");
+
+        JbJogar.setText("Jogar");
+        JbJogar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JbJogarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(123, 123, 123)
+                .addGap(135, 135, 135)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(JbTesoura)
-                    .addComponent(JbPapel)
-                    .addComponent(JbPedra)
-                    .addComponent(jLabel1))
-                .addContainerGap(146, Short.MAX_VALUE))
+                    .addComponent(JrTesoura)
+                    .addComponent(JrPapel)
+                    .addComponent(JbJogar)
+                    .addComponent(jLabel1)
+                    .addComponent(JrPedra))
+                .addContainerGap(134, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -62,20 +72,92 @@ public class TelaPrincipal extends javax.swing.JFrame {
                 .addGap(58, 58, 58)
                 .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(JbPedra)
+                .addComponent(JrPedra)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(JbPapel)
+                .addComponent(JrPapel)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(JbTesoura)
-                .addContainerGap(139, Short.MAX_VALUE))
+                .addComponent(JrTesoura)
+                .addGap(18, 18, 18)
+                .addComponent(JbJogar)
+                .addContainerGap(98, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void JbPedraActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JbPedraActionPerformed
+    private void JrPedraActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JrPedraActionPerformed
 
-    }//GEN-LAST:event_JbPedraActionPerformed
+    }//GEN-LAST:event_JrPedraActionPerformed
+
+    private void JbJogarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JbJogarActionPerformed
+        int numeroAleatorio;
+
+        boolean pedra = (boolean) JrPedra.isSelected();
+        boolean papel = (boolean) JrPapel.isSelected();
+        boolean tesoura = (boolean) JrTesoura.isSelected();
+
+        if (pedra) {
+            JrPedra.setSelected(false);
+            numeroAleatorio = radInt(0, 2);
+            if (numeroAleatorio == 0) {
+                JrPedra.doClick();
+                JOptionPane.showMessageDialog(null, "Empate");
+                JrPedra.setSelected(false);
+                
+            } else if (numeroAleatorio == 1) {
+                JrPapel.doClick();
+                JOptionPane.showMessageDialog(null, "Perdeu");
+                JrPapel.setSelected(false);
+                
+            } else if (numeroAleatorio == 2) {
+                JrTesoura.doClick();
+                JOptionPane.showMessageDialog(null, "Venceu");
+                JrTesoura.setSelected(false);
+            }
+        }
+
+        if (papel) {
+            JrPapel.setSelected(false);
+            numeroAleatorio = radInt(0, 2);
+            if (numeroAleatorio == 0) {
+                JrPedra.doClick();
+                JOptionPane.showMessageDialog(null, "Venceu");
+                JrPedra.setSelected(false);
+                
+            } else if (numeroAleatorio == 1) {
+                JrPapel.doClick();
+                JOptionPane.showMessageDialog(null, "Empate");
+                JrPapel.setSelected(false);
+                
+            } else if (numeroAleatorio == 2) {
+                JrTesoura.doClick();
+                JOptionPane.showMessageDialog(null, "Perdeu");
+                JrTesoura.setSelected(false);
+            }
+        }
+
+        if (tesoura) {
+            JrTesoura.setSelected(false);
+            numeroAleatorio = radInt(0, 2);
+            if (numeroAleatorio == 0) {
+                JrPedra.doClick();
+                JOptionPane.showMessageDialog(null, "Perdeu");
+                JrPedra.setSelected(false);
+                
+            } else if (numeroAleatorio == 1) {
+                JrPapel.doClick();
+                JOptionPane.showMessageDialog(null, "Venceu");
+                JrPapel.setSelected(false);
+                
+            } else if (numeroAleatorio == 2) {
+                JrTesoura.doClick();
+                JOptionPane.showMessageDialog(null, "Empatou");
+                JrTesoura.setSelected(false);
+            }
+        }
+
+
+    }//GEN-LAST:event_JbJogarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -118,29 +200,12 @@ public class TelaPrincipal extends javax.swing.JFrame {
         return randomNum;
     }
 
-    public void jogo() {
-        int numeroAleatorio;
-         numeroAleatorio = radInt(0, 2);
-        boolean pedra = (boolean) JbPedra.isSelected();
-        boolean papel = (boolean) JbPapel.isSelected();
-        boolean tesoura = (boolean) JbTesoura.isSelected();
 
-        if (pedra) {
-            
-
-            if (numeroAleatorio == 1) {
-                JbPapel.doClick();
-            } else if (numeroAleatorio == 2) {
-
-                JbTesoura.doClick();
-            }
-
-        }
-    }
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JRadioButton JbPapel;
-    private javax.swing.JRadioButton JbPedra;
-    private javax.swing.JRadioButton JbTesoura;
+    private javax.swing.JButton JbJogar;
+    private javax.swing.JRadioButton JrPapel;
+    private javax.swing.JRadioButton JrPedra;
+    private javax.swing.JRadioButton JrTesoura;
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
