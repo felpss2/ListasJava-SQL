ecca9472440f99a4b8f43de7fe6365c7a5307c6c
05136425016 <05136425016@senacrs.edu.br>
2024-07-11 19:21:15 -0300

    commitando

---
+++ ConsultaDeSintomas/src/main/java/com/mycompany/consultadesintomas/ConsultaDeSintomas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.consultadesintomas;
+
+/**
+ *
+ * @author 05136425016
+ */
+public class ConsultaDeSintomas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
